91307bb46115db60143ff5832dc6597dd1083570
Brian Inguito <vincentvangoess@gmail.com>
2024-10-19 00:13:56 +0800

    navbar changes

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,43 +2,34 @@
 import { ref } from 'vue'
 
 const theme = ref('light')
-
-function onClick() {
-  theme.value = theme.value === 'light' ? 'dark' : 'light'
-}
 </script>
 
 <template>
   <v-responsive class="border rounded">
     <v-app :theme="theme">
       <v-app-bar :style="{ backgroundColor: '#111703' }" class="px-3">
-        <v-toolbar-title>CampusThreads</v-toolbar-title>
-        <v-btn text>Home</v-btn>
-        <v-btn text>Departments</v-btn>
-        <v-btn text>Shop all</v-btn>
-        <v-btn text>About</v-btn>
+        <v-toolbar-title class="mr-8" style="color: white"
+          >CampusThreads</v-toolbar-title
+        >
 
+        <v-btn text class="mr-1" style="color: white">Home</v-btn>
+        <v-btn text class="mr-1" style="color: white">Departments</v-btn>
+        <v-btn text class="mr-1" style="color: white">Shop all</v-btn>
+        <v-btn text class="mr-1" style="color: white">About</v-btn>
+        <v-spacer></v-spacer>
         <v-text-field
+          class="mx-4"
+          style="color: white"
+          prepend-inner-icon="mdi-magnify"
           placeholder="search"
-          append-icon="mdi-magnify"
           hide-details
           dense
           solo
         ></v-text-field>
-        <v-btn icon>
-          <v-icon>mdi-account</v-icon>
+        <v-btn class="mr-5 ml-5" style="color: white">
+          <v-icon left>mdi-account</v-icon>
+          Log In
         </v-btn>
-        <v-btn icon>
-          <v-icon>mdi-cart</v-icon>
-        </v-btn>
-        <v-btn
-          :prepend-icon="
-            theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'
-          "
-          text="Toggle Theme"
-          slim
-          @click="onClick"
-        ></v-btn>
       </v-app-bar>
 
       <v-main>
@@ -68,7 +59,7 @@ function onClick() {
                       type="password"
                     ></v-text-field>
 
-                    <v-btn class="mt-2" type="submit" block>Submit</v-btn>
+                    <v-btn class="mt-2" type="submit" block>Log In</v-btn>
                   </v-form>
                 </v-card-text>
               </v-card>
