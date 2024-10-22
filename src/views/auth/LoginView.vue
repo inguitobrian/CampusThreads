28dcc1ad6f6cacaeb1193e8d9928b7dd02a5189a
reymartjohneva <reymarteva2@example.com>
2024-10-22 10:26:30 +0800

    menu icon, navbar and logo

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -2,37 +2,77 @@
 import { ref } from 'vue'
 
 const theme = ref('light')
-const visible = ref(false) 
+const visible = ref(false)
 const isAdmin = ref(false)
-const rememberMe = ref(false) 
+const rememberMe = ref(false)
+const drawer = ref(false) // for the navigation drawer on mobile screens
 </script>
 
 <template>
   <v-responsive class="border rounded">
     <v-app :theme="theme" style="background-color: #e1ebdc; font-family: 'Roboto', sans-serif;">
-      <v-app-bar :style="{ backgroundColor: '#111703' }" class="px-3">
-        <v-toolbar-title class="mr-8" style="color: white; font-family: 'Montserrat', sans-serif;">CampusThreads</v-toolbar-title>
+      <!-- App Bar -->
+      <v-app-bar :style="{ backgroundColor: '#111703' }" class="px-3 nav-bar">
+        <!-- Mobile View: Hamburger Icon -->
+        <v-app-bar-nav-icon @click="drawer = !drawer" style="color: white" class="d-lg-none"></v-app-bar-nav-icon>
+        
+        <!-- Toolbar Title -->
+        <v-toolbar-title style="color: white; font-family: 'Montserrat', sans-serif;">
+          CampusThreads
+        </v-toolbar-title>
 
-        <v-btn text class="mr-1" style="color: white">Home</v-btn>
-        <v-btn text class="mr-1" style="color: white">Departments</v-btn>
-        <v-btn text class="mr-1" style="color: white">Shop all</v-btn>
-        <v-btn text class="mr-1" style="color: white">About</v-btn>
         <v-spacer></v-spacer>
-        <v-text-field
-          class="mx-4"
-          style="color: white"
-          prepend-inner-icon="mdi-magnify"
-          placeholder="search"
-          hide-details
-          dense
-          solo
-        ></v-text-field>
+
+        <!-- Desktop View: Nav Buttons -->
+        <div class="d-none d-lg-flex nav-buttons">
+          <v-btn text class="mr-1" style="color: white">Home</v-btn>
+          <v-btn text class="mr-1" style="color: white">Departments</v-btn>
+          <v-btn text class="mr-1" style="color: white">Shop all</v-btn>
+          <v-btn text class="mr-1" style="color: white">About</v-btn>
+          <!-- Search Field -->
+          <v-text-field
+            class="mx-4 search-bar"
+            style="color: white"
+            prepend-inner-icon="mdi-magnify"
+            placeholder="Search"
+            hide-details
+            dense
+            solo
+          ></v-text-field>
+        </div>
+
+        <!-- Log In Button for all sizes -->
         <v-btn class="mr-5 ml-5" style="color: white">
           <v-icon left>mdi-account</v-icon>
           Log In
         </v-btn>
       </v-app-bar>
 
+      <!-- Mobile Navigation Drawer -->
+      <v-navigation-drawer v-model="drawer" app temporary>
+        <v-list dense>
+          <v-list-item>
+            <v-list-item-title class="text-h6">Menu</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-content>Home</v-list-item-content>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-content>Departments</v-list-item-content>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-content>Shop all</v-list-item-content>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-content>About</v-list-item-content>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-content>Log In</v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <!-- Main Content -->
       <v-main>
         <v-container fluid>
           <v-row justify="end">
@@ -55,18 +95,21 @@ const rememberMe = ref(false)
             <v-col cols="12" md="6" class="d-flex justify-center">
               <v-card
                 class="text-center rounded-xl elevation-10"
-                prepend-icon="$vuetify"
                 style="max-width: 400px"
                 height="auto"
               >
-               
-                <v-card-title
-                  class="text-h4 font-weight-black light-green-darken-4"
-                >
+                <v-img
+                  src="public\i_logo.png" 
+                  alt="Custom Logo"
+                  class="mb-4"
+                  max-width="40" 
+                  contain
+                ></v-img>
+
+                <v-card-title class="text-h4 font-weight-black light-green-darken-4">
                   <span>Welcome Back!</span>
                 </v-card-title>
 
-               
                 <v-card-text class="d-flex justify-center mb-4">
                   <v-btn @click="isAdmin = false" :class="{'bg-student': !isAdmin}" style="color: #cdd7bf;">Student</v-btn>
                   <v-btn @click="isAdmin = true" :class="{'bg-admin': isAdmin}" style="color: #cdd7bf;">Admin</v-btn>
@@ -76,7 +119,6 @@ const rememberMe = ref(false)
                   {{ isAdmin ? 'Securely log in to your CampusThreads Admin account.' : 'Securely log in to your CampusThreads Student account.' }}
                 </v-card-text>
 
-             
                 <v-card-text>
                   <v-form fast-fail @submit.prevent>
                     <v-text-field
@@ -110,8 +152,7 @@ const rememberMe = ref(false)
                       size="large"
                       type="submit"
                       block
-                      style="color: ;"
-                      >Log In
+                    >Log In
                     </v-btn>
                     <v-card-text class="text-center">
                       <a
@@ -120,21 +161,19 @@ const rememberMe = ref(false)
                         rel="noopener noreferrer"
                         target="_blank"
                       >
-                      Don't have an account yet? Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
+                        Don't have an account yet? Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
                       </a>
                     </v-card-text>
                   </v-form>
                 </v-card-text>
 
-              
-                <v-card-actions>
-                
-                </v-card-actions>
+                <v-card-actions></v-card-actions>
               </v-card>
             </v-col>
           </v-row>
         </v-container>
       </v-main>
+
       <v-footer border style="background-color: #111703; color: white">
         2024 - CampusThreads
       </v-footer>
@@ -155,8 +194,30 @@ const rememberMe = ref(false)
   background-color: #acbd99; 
 }
 
-
 .v-input--selection-controls .v-input--checkbox .v-input__control .v-input__slot {
   color: #63794e; 
+}
+
+.nav-bar {
+  min-height: 64px; 
+  padding: 0 16px; 
+  display: flex;
+  align-items: center;
+}
+
+.nav-buttons {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+
+.search-bar {
+  min-width: 300px;
+  max-width: 500px;
+  margin-right: 20px; 
+}
+
+.search-bar {
+  min-height: 40px;
 }
 </style>
