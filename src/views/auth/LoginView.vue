22911426ff367fe7d3678d4f133d17cb08ab033d
reymartjohneva <reymarteva2@example.com>
2024-10-22 09:36:34 +0800

    Text and color improved

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -2,15 +2,16 @@
 import { ref } from 'vue'
 
 const theme = ref('light')
+const visible = ref(false) 
+const isAdmin = ref(false)
+const rememberMe = ref(false) 
 </script>
 
 <template>
   <v-responsive class="border rounded">
-    <v-app :theme="theme" style="background-color: #e1ebdc">
+    <v-app :theme="theme" style="background-color: #e1ebdc; font-family: 'Roboto', sans-serif;">
       <v-app-bar :style="{ backgroundColor: '#111703' }" class="px-3">
-        <v-toolbar-title class="mr-8" style="color: white"
-          >CampusThreads</v-toolbar-title
-        >
+        <v-toolbar-title class="mr-8" style="color: white; font-family: 'Montserrat', sans-serif;">CampusThreads</v-toolbar-title>
 
         <v-btn text class="mr-1" style="color: white">Home</v-btn>
         <v-btn text class="mr-1" style="color: white">Departments</v-btn>
@@ -58,20 +59,24 @@ const theme = ref('light')
                 style="max-width: 400px"
                 height="auto"
               >
-                <!-- Card Title -->
+               
                 <v-card-title
                   class="text-h4 font-weight-black light-green-darken-4"
                 >
                   <span>Welcome Back!</span>
                 </v-card-title>
 
-                <!-- Card Text -->
-                <v-card-text class="bg-surface-white mx-8 text-center ml-14">
-                  Securely log in to your CampusThreads account and enjoy
-                  personalized shopping.
+               
+                <v-card-text class="d-flex justify-center mb-4">
+                  <v-btn @click="isAdmin = false" :class="{'bg-student': !isAdmin}" style="color: #cdd7bf;">Student</v-btn>
+                  <v-btn @click="isAdmin = true" :class="{'bg-admin': isAdmin}" style="color: #cdd7bf;">Admin</v-btn>
                 </v-card-text>
 
-                <!-- Form Section -->
+                <v-card-text class="bg-surface-white mx-8 text-center ml-14">
+                  {{ isAdmin ? 'Securely log in to your CampusThreads Admin account.' : 'Securely log in to your CampusThreads Student account.' }}
+                </v-card-text>
+
+             
                 <v-card-text>
                   <v-form fast-fail @submit.prevent>
                     <v-text-field
@@ -84,25 +89,28 @@ const theme = ref('light')
                       :type="visible ? 'text' : 'password'"
                       label="Password"
                       prepend-inner-icon="mdi-lock-outline"
-                      type="password"
                       @click:append-inner="visible = !visible"
-                    >
-                    </v-text-field>
-                    <a
-                      class="text-caption text-decoration-none text-blue"
-                      style="display: block; text-align: right"
-                      href="#"
-                      rel="noopener noreferrer"
-                      target="_blank"
-                    >
-                      Forgot login password?</a
-                    >
+                    ></v-text-field>
+
+                    <div class="d-flex align-center justify-between mb-2">
+                      <v-checkbox v-model="rememberMe" label="Remember Me" style="color: #63794e;"></v-checkbox>
+                      <a
+                        class="text-caption text-decoration-none text-blue"
+                        href="#"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        style="margin-left: auto;"
+                      >
+                        <u>Forgot password?</u>
+                      </a>
+                    </div>
 
                     <v-btn
-                      class="mt-2 mb-8 text-h6 font-weight-bold"
+                      class="mt-2 mb-8 text-h6 font-weight-bold bg-login"
                       size="large"
                       type="submit"
                       block
+                      style="color: ;"
                       >Log In
                     </v-btn>
                     <v-card-text class="text-center">
@@ -112,15 +120,15 @@ const theme = ref('light')
                         rel="noopener noreferrer"
                         target="_blank"
                       >
-                        Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
+                      Don't have an account yet? Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
                       </a>
                     </v-card-text>
                   </v-form>
                 </v-card-text>
 
-                <!-- Card Actions -->
+              
                 <v-card-actions>
-                  <!-- You can add any action buttons or links here if needed -->
+                
                 </v-card-actions>
               </v-card>
             </v-col>
@@ -133,3 +141,22 @@ const theme = ref('light')
     </v-app>
   </v-responsive>
 </template>
+
+<style>
+.bg-login {
+  background-color: #71885a;
+}
+
+.bg-student {
+  background-color: #8fa477; 
+}
+
+.bg-admin {
+  background-color: #acbd99; 
+}
+
+
+.v-input--selection-controls .v-input--checkbox .v-input__control .v-input__slot {
+  color: #63794e; 
+}
+</style>
