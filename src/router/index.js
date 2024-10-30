3012bb73db5464c88f382538defc10dd2fbc487b
reymartjohneva <reymarteva2@example.com>
2024-10-30 18:46:53 +0800

    Ccis page not completed

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/auth/LoginView.vue'
 import RegisterView from '@/views/auth/RegisterView.vue'
 import HomePage from '@/views/auth/HomePage.vue'
+import CcisPage from '@/views/auth/CcisPage.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -16,9 +17,14 @@ const router = createRouter({
       component: RegisterView,
     },
     {
-      path: '/',
+      path: '/home',
       name: 'home', 
       component: HomePage,
+    },
+    {
+      path: '/',
+      name: 'ccis',
+      component: CcisPage,
     },
   ],
 })
