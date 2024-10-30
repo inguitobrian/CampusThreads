55d6286359b57acfc033b72171fc29e97d8e4967
reymartjohneva <reymarteva2@example.com>
2024-10-30 18:52:38 +0800

    Caa page not complete

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import LoginView from '@/views/auth/LoginView.vue'
 import RegisterView from '@/views/auth/RegisterView.vue'
 import HomePage from '@/views/auth/HomePage.vue'
 import CcisPage from '@/views/auth/CcisPage.vue'
+import CaaPage from '@/views/auth/CaaPage.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -22,10 +23,15 @@ const router = createRouter({
       component: HomePage,
     },
     {
-      path: '/',
+      path: '/ccis',
       name: 'ccis',
       component: CcisPage,
     },
+    {
+      path: '/',
+      name: 'caa',
+      component: CaaPage,
+    }
   ],
 })
 
